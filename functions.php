d0cc20d9774878cccc58354c2ee76886d755085e
sebastian <sebastiangimenezomar@gmail.com>
2023-06-16 15:33:02 -0300

    css enqueue function

--- functions.php
+++ functions.php
@@ -4,3 +4,14 @@
  * 
  * @package ct_sebastian
  */ 
+
+
+// enqueing css function 
+function ct_sebastian_enqueue_scripts() {
+
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [] , filemtime( get_template_directory() . '/style.css' ), 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'ct_sebastian_enqueue_scripts' );
+
+?>
--- header.php
+++ header.php
@@ -12,5 +12,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body>
+
+<!-- body_class is used to add classes to the pages (you can pass your own class using [body_class('sample-class')] )   -->
+
+<body <?php body_class();?>>
+
+<!-- use wp_body_open to add script in the start of body  -->
+
+    <?php wp_body_open(); ?>
+
     <header>header</header>
