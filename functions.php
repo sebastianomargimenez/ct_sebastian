9dcc135efc0d394e0690fcfd87a2e5dfe84a066f
Sebastián Giménez <87837358+sebastianomargimenez@users.noreply.github.com>
2023-06-19 13:56:41 -0300

    Add files via upload

--- functions.php
+++ functions.php
@@ -9,13 +9,26 @@
 // enqueing css function 
 function ct_sebastian_enqueue_scripts() {
 
+    // register styles
     wp_register_style('style-css', get_stylesheet_uri(), [] , filemtime( get_template_directory() . '/style.css' ), 'all' );
 
+    wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [] , false, 'all' );
+
+
+    // register scripts
     wp_register_script( 'main-js', get_template_directory_uri(  ) . '/assets/main.js', [], filemtime( get_template_directory() . '/assets/main.js' ), true  );
 
+    wp_register_script( 'bootstrap-js', get_template_directory_uri(  ) . '/assets/src/library/css/bootstrap.min.js', [ 'jQuery' ],false ,true  );
+
+    // enqueue styles
     wp_enqueue_style( 'style-css' );
 
-    wp_enqueue_script( 'main.js' );
+    wp_enqueue_style( 'bootstrap-css' );
+    
+    //enqueue scripts
+    wp_enqueue_script( 'main-js' );
+
+    wp_enqueue_script( 'bootstrap-js' );
     
 }
 
